37182976291219152646d7e6db21d97140b89a50
Aldino <grasepta@gmail.com>
2020-07-18 20:27:33 +0700

    Initial Commit

---
+++ Rindang/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Rindang
+//
+//  Created by Aldino Grasepta on 18/07/20.
+//  Copyright © 2020 Aldino Grasepta. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
